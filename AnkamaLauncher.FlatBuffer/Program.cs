3349c734825c78de68844a7fe6b265ed225085a1
Alpa <alpaoff@gmail.com>
2022-12-16 00:32:20 +0100

    Initial commit

---
+++ AnkamaLauncher.FlatBuffer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+using Google.FlatBuffers;
+
+var httpClient = new HttpClient();
+var metaFile = await httpClient.GetByteArrayAsync("https://cytrus.cdn.ankama.com/dofus/releases/main/windows/6.0_2.66.1.8.manifest");
+
+var test = Manifest.GetRootAsManifest(new ByteBuffer(metaFile));
+Console.WriteLine(test.FragmentsLength);
+
+var mt = test.UnPack();
+
+System.IO.File.WriteAllText("manifest.json", JsonSerializer.Serialize(mt, new JsonSerializerOptions() { WriteIndented = true}));
